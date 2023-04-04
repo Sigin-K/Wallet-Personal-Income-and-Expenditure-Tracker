3e53699bb1b33bf2fd705606fa1acfa507245f50
Sigin K <107768272+Sigin-K@users.noreply.github.com>
2023-04-04 18:05:04 +0530

    Delete lib directory

--- lib/models/transaction_detail.dart
+++
@@ -1,9 +0,0 @@
-class TransactionDetail{
-  final int amount;
-  final DateTime date;
-  final String note;
-  final String type;
-
-TransactionDetail(this.amount,this.date,this.note,this.type);
-
-}
